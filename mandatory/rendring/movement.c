6256a552751c7321c2f86cd7bd0d5a5b83a7140d
Adil Ferryat <aferryat@e3r9p3.1337.ma>
2025-10-27 00:18:30 +0100

    wwww

--- mandatory/rendring/movement.c
+++ mandatory/rendring/movement.c
@@ -6,7 +6,7 @@
 /*   By: aferryat <aferryat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 17:02:01 by aferryat          #+#    #+#             */
-/*   Updated: 2025/10/26 17:45:23 by aferryat         ###   ########.fr       */
+/*   Updated: 2025/10/27 00:17:43 by aferryat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,9 @@ void	go_backward(t_player *player)
 void	turn(t_player	*player, int rl)
 {
 	if (rl == 1)
-		player->radiant -= degree_to_radiant(10);
+		player->radiant -= degree_to_radiant(8);
 	else
-		player->radiant += degree_to_radiant(10);
+		player->radiant += degree_to_radiant(8);
 }
 
 void	left_right(t_player	*player, int rl)
